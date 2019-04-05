3c82c57fcd1de7ba7bf4cf88ceaff56f3c0f7763
Jason YI Jiang <jiangkaiyi@jiangkaiyide-MacBook-Pro.local>
2019-04-06 01:32:33 +0800

    update main.py data[0]

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.getRevenue import getRevenue
 
 if __name__ == "__main__":
+  year = '2018'
+  month = '11'
   _revenue = getRevenue('1','2018', '11')
   totalPage = _revenue.getPageCount()
   for i in range(1, totalPage + 1):
@@ -10,4 +12,5 @@ if __name__ == "__main__":
   
   allData = _revenue.getDataArr()
   for data in allData:
-    print(data[1], data[2], data[3], data[4], data[5], data[6])
+    #id, name, permonth, permonthchangerate, permonthgrowthrate, cumulation, cumulationrate
+    print(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
